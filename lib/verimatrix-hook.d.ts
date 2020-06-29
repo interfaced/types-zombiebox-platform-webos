bdc8a124020a05e496b38f18229d55631deadad1
reefridge <reefridgerator@gmail.com>
2020-06-29 18:07:08 +0700

    update types

--- lib/verimatrix-hook.d.ts
+++ lib/verimatrix-hook.d.ts
@@ -2,5 +2,5 @@ import VerimatrixClient from 'zombiebox/zb/device/drm/verimatrix-client';
 import AbstractDrmHook from './abstract-drm-hook';
 
 export default class VerimatrixHook extends AbstractDrmHook {
-    constructor(client: VerimatrixClient);
+	constructor(client: VerimatrixClient);
 }
--- lib/video.d.ts
+++ lib/video.d.ts
@@ -4,18 +4,18 @@ import IViewPort from 'zombiebox/zb/device/interfaces/i-view-port';
 import MediaOption from './media-option';
 
 export default class Video extends HTML5Video {
-  protected _mimeType: string | null;
-  protected _mediaOption: MediaOption;
-  constructor(rect: Rect);
-  play(url: string, startFrom?: number): void;
-  setMimeType(mimeType?: string): void;
-  setMediaOption(mediaOption: MediaOption): void;
-  protected _createVideoObject(): HTMLVideoElement;
-  protected _createViewPort(containerRect: Rect): IViewPort;
-  protected _initEvents(): void;
-  protected _replaceEventListener(event: string, listener: () => void): void;
-  protected _applyTypeAttribute(mediaOption: MediaOption): void;
-  protected _deepAssign(target: object, source: object): object;
-  static createStartTimeMediaOption(startTime: number): MediaOption;
-  static serializeMediaOption(mediaOption: MediaOption): string;
+	protected _mimeType: string | null;
+	protected _mediaOption: MediaOption;
+	constructor(rect: Rect);
+	play(url: string, startFrom?: number): void;
+	setMimeType(mimeType?: string): void;
+	setMediaOption(mediaOption: MediaOption): void;
+	protected _createVideoObject(): HTMLVideoElement;
+	protected _createViewPort(containerRect: Rect): IViewPort;
+	protected _initEvents(): void;
+	protected _replaceEventListener(event: string, listener: () => void): void;
+	protected _applyTypeAttribute(mediaOption: MediaOption): void;
+	protected _deepAssign(target: object, source: object): object;
+	static createStartTimeMediaOption(startTime: number): MediaOption;
+	static serializeMediaOption(mediaOption: MediaOption): string;
 }
